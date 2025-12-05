03c420a36e3acbc85e147d48b184cb7732c7a384
Jihoon-Yoon96 <03470@naver.com>
2025-12-06 01:45:09 +0900

    배포 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,23 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
 
 export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-    base: '/front_7th_chapter3-2/',
-    build: {
-      rollupOptions: {
-        input: {
-          // 각 HTML 파일의 진입점을 정의합니다.
-          basic: resolve(__dirname, 'index.basic.html'),
-          advanced: resolve(__dirname, 'index.advanced.html'),
-          origin: resolve(__dirname, 'index.origin.html'), // 필요하면 포함
+    defineConfig({
+        plugins: [react()],
+        base: process.env.VITE_BASE_PATH || '/',
+        build: {
+            rollupOptions: {
+                input: ['./index.advanced.html', './index.basic.html'],
+            },
+            outDir: 'dist',
         },
-      },
-    },
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
-    },
-  })
-)
+    }),
+    defineTestConfig({
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: './src/setupTests.ts',
+        },
+    }),
+);
